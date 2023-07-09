7120a5dad3349ebbcc08188618a3f5bad928e455
christianwaldmann <christianwaldmann>
2023-07-09 21:54:40 +0200

    change docs from redoc to swagger ui

--- api/urls.py
+++ api/urls.py
@@ -31,8 +31,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin', admin.site.urls),
-    path('doc', schema_view.with_ui('redoc',
-                                         cache_timeout=0), name="schema-redoc"),
+    path('docs', schema_view.with_ui('swagger',
+                                         cache_timeout=0), name="schema-swagger-ui"),
     path('', include('bookmarks.urls')),
     path('', include('accounts.urls')),
 ]
